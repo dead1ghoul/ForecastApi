0e025a235a8e00d2bc23405e2927a405b24d7003
dead1ghoul <115005232+dead1ghoul@users.noreply.github.com>
2024-02-15 17:06:00 +0300

    TBot Init

--- Program.cs
+++ Program.cs
@@ -1,43 +1,65 @@
-﻿using System.Net;
-using System.IO;
-using System;
-using System.Net.Http;
+﻿using MailKit;
 using Newtonsoft.Json;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Requests;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using WethaBot;
 
-namespace WethaBot
+namespace WeatherBot
 {
-
     class Program
     {
         private static TelegramBotClient botClient;
-
+        public static string response;
         static void Main(string[] args)
         {
+            var client = new TelegramBotClient("6499335199:AAGDmg7pg2VCwdK2vQZ335k4hAvBmfzMsoM");
 
-            Console.WriteLine("Enter the city: ");
-            string cityName = Console.ReadLine();
+            client.StartReceiving(Update, Error);
 
-            // Адрес API для получения погоды через координаты
-            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=03a6166867875433b671f0b4a50744fc";
+            Console.WriteLine("Bot started. Press any key to stop...");
+            Console.ReadLine();
 
-
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(apiUrl);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            string response;
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
-            { 
-                response = streamReader.ReadToEnd();
-            }
-       
-            WeatherResp weatherResp = JsonConvert.DeserializeObject<WeatherResp>(response);
-            Console.WriteLine($"Temperature in {weatherResp.Name} : {weatherResp.Main.Temp}°C");
         }
 
-        
+
+        async public static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine(message.Text);
+                string city = message.Text;
+                if (message.Text.ToLower().Contains("/weather"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Введите название города:");
+                    
+                }
+            }
+        }
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetWeather(string cityName)
+        {
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=YOUR_API_KEY";
+
+            var http = new HttpClient();
+            var response = http.GetStringAsync(apiUrl).Result;
+
+            WeatherResp weatherResp = JsonConvert.DeserializeObject<WeatherResp>(response);
+            return $"Temperature in {weatherResp.Name}: {weatherResp.Main.Temp}°C";
+        }
 
     }
-
 }
