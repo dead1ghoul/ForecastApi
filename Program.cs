08c6e3421a6da9235c5d52e4b67c16ca4fd123c6
dead1ghoul <115005232+dead1ghoul@users.noreply.github.com>
2024-02-20 23:09:46 +0300

    Request processing + error tolerance

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
-﻿using MailKit;
+﻿
 using Newtonsoft.Json;
 using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
-using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
-using WethaBot;
+using WethaBot.Models;
 
 namespace WeatherBot
 {
     class Program
     {
         private static TelegramBotClient botClient;
-        public static string response;
+        static string _lastCommand;
         static void Main(string[] args)
         {
             var client = new TelegramBotClient("6499335199:AAGDmg7pg2VCwdK2vQZ335k4hAvBmfzMsoM");
@@ -31,17 +26,36 @@ namespace WeatherBot
         }
 
 
-        async public static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
-        {
+         public static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+         {
             var message = update.Message;
-            if (message.Text != null)
+            if (message.Text == "/start")
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Введите /weather , чтобы узнать погоду:");
+            }
+            if (message != null && message.Text != null)
             {
                 Console.WriteLine(message.Text);
-                string city = message.Text;
+                
                 if (message.Text.ToLower().Contains("/weather"))
                 {
                     await botClient.SendTextMessageAsync(message.Chat.Id, "Введите название города:");
+                    _lastCommand = "/weather";
                     
+                }
+
+                if (_lastCommand == "/weather" && message.Text!= "/weather")
+                {
+                    _lastCommand = string.Empty;
+                    string city = message.Text;
+                    var weatherResult = GetWeather(city);
+                    if (weatherResult != null)
+                    {
+                        Console.WriteLine(weatherResult);
+                        await botClient.SendTextMessageAsync(message.Chat.Id, weatherResult); 
+                    }
+                    else
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "retype");
                 }
             }
         }
@@ -52,14 +66,35 @@ namespace WeatherBot
 
         public static string GetWeather(string cityName)
         {
-            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=YOUR_API_KEY";
+            try
+            {
+                string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=c14343f016c933c8bdc7491701478f9c";
 
-            var http = new HttpClient();
-            var response = http.GetStringAsync(apiUrl).Result;
+                var http = new HttpClient();
+                var response = http.GetStringAsync(apiUrl).Result;
 
-            WeatherResp weatherResp = JsonConvert.DeserializeObject<WeatherResp>(response);
-            return $"Temperature in {weatherResp.Name}: {weatherResp.Main.Temp}°C";
+                if (response.Contains("404 Not Found"))
+                {
+                    return "Город не найден";
+                }
+
+                СityWeather cityWeather = JsonConvert.DeserializeObject<СityWeather>(response);
+                return $"Температура в {cityWeather.Name}: {cityWeather.Main.Temp}°C";
+            }
+            catch (HttpRequestException)
+            {
+                return "Failed to connect to weather service";
+            }
+            catch (JsonException)
+            {
+                return "Failed to deserialize weather data";
+            }
+            catch (Exception ex)
+            {
+                return $"Такого города не существует, введите реальный: {ex.Message}";
+            }
         }
-
     }
+
+    
 }
