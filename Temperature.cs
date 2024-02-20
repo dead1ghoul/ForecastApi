43ceba8bd6a62902b68c8c9c25a37358cee02654
dead1ghoul <115005232+dead1ghoul@users.noreply.github.com>
2024-02-20 23:11:58 +0300

    Models

---
+++ Temperature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WethaBot.Models
+{
+    public class Temperature
+    {
+        public float Temp { get; set; }
+    }
+}
---
+++ СityWeather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WethaBot.Models
+{
+    public class СityWeather
+    {
+
+        public Temperature Main { get; set; }
+        public string Name { get; set; }
+    }
+}
